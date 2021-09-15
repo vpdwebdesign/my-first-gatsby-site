be8987c87d1f40bafccd0df5776693d357d46c50
vpdwebdesign <vpdwebdesign@gmail.com>
2021-09-15 18:57:56 +0000

    First ever commit

---
+++ README.md
@@ -0,0 +1,1 @@
+For the official Gatsby tutorial
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <h1>Congratulations</h1>
+    </main>
+  )
+}
+
+export default IndexPage
